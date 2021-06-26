6edccddcd130202d381c6561741f0ebff11dd405
Victor Raposo <69219137+vhraposo@users.noreply.github.com>
2021-06-26 03:41:49 -0300

    Add files via upload

--- ROCKETQ/src/controllers/RoomController.js
+++ ROCKETQ/src/controllers/RoomController.js
@@ -5,23 +5,52 @@ module.exports = {
         const db = await Database()
         const pass = req.body.password
         let roomId 
+        let isRoom = true
 
+        while(isRoom){
+              //*Gera o npúmero da sala/*
         for(var i = 0; i < 6; i++){
             i == 0 ? roomId = Math.floor(Math.random() *10).toString() :
              roomId += Math.floor(Math.random() *10).toString()
-        }
+         }
 
-     
+         //  Verificar se esse número já existe
+        const roomsExistIds = await db.all(`SELECT id FROM rooms`)
+        isRoom = roomsExistIds.some(roomExistId => roomExistId === roomId)
+         if(!isRoom) {
+         // Insere a sala no banco *//
         await db.run(`INSERT INTO rooms (
             id,
             pass
-        ) VALUES(
+            ) VALUES(
             ${parseInt(roomId)},
             ${pass}
-        )`)
+            )`)
+            }
+        }
+          
+         await db.close(``)
 
-        await db.close(``)
+         res.redirect(`/room/${roomId}`)
+    },
+    async open(req, res){
+        const db = await Database()
+        const roomId = req.params.room
+        const questions = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 0`)
+        const questionsRead = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 1`)
+        let isNoQuestions 
+        if(questions.length ==0){
+            if(questionsRead.length ==0){
+                isNoQuestions = true
+            }
+        }
 
-        res.redirect(`/room/${roomId}`)
+
+        res.render("room",{roomId : roomId, questions: questions, questionsRead: questionsRead, isNoQuestions})
+    },
+    enter(req, res){
+       const roomId = req.body.roomId
+
+       res.redirect(`/room/${roomId}`)
     }
 }
